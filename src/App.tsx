2667a54b7b5a9a3589cb2ec36eae391db0550137
WindrunnerMax <651525974@qq.com>
2022-06-20 08:56:01 +0800

    主题切换

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.scss";
 import SlateDocEditor from "src/views";
 import { useRef } from "react";
 import { cs } from "./utils/classnames";
+import { IconFile } from "@arco-design/web-react/icon";
+import { ThemeAction } from "./components/theme-action";
 
 export const App: React.FC = () => {
   const isRender = useRef(false);
@@ -11,7 +13,13 @@ export const App: React.FC = () => {
   return (
     <div className="doc-editor-container">
       <div className="header">
-        <span>Editor</span>
+        <span className="left">Editor</span>
+        <div className="right">
+          <div>
+            <IconFile onClick={() => window.open("./#render", "_blank")} />
+          </div>
+          <ThemeAction />
+        </div>
       </div>
       <div className="gap"></div>
       <div className={cs("editor", isRender.current && "render")}>
