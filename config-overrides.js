dec284eb75b9335e75da3f27a673eada0bd57688
WindrunnerMax <651525974@qq.com>
2022-10-23 10:06:06 +0800

    feat: esbuild dev mode

--- config-overrides.js
+++ config-overrides.js
@@ -1,51 +1,36 @@
 const { override, disableEsLint, addLessLoader } = require("customize-cra");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-// const { paths: rewiredPaths } = require("react-app-rewired");
-// const { scriptVersion } = rewiredPaths;
-// const paths = require(`${scriptVersion}/config/paths`);
+const rewiredESbuild = require("react-app-rewired-esbuild");
 // https://github.com/arackaf/customize-cra
 
 const configWebpackPlugins = () => config => {
   // 太卡关闭一些插件
-  // 关闭`ESLINT`的插件 在`VSCode`校验
+  // 关闭`ESLINT`的插件 -> 在`VSCode`校验
   // 关闭`CaseSensitivePathsPlugin`插件
   // 关闭`IgnorePlugin`插件
-  // 关闭`TS`校验插件 在`VSCode`校验
   config.plugins = config.plugins.filter(
     plugin =>
       plugin.constructor.name !== "ESLintWebpackPlugin" &&
       plugin.constructor.name !== "CaseSensitivePathsPlugin" &&
       plugin.constructor.name !== "IgnorePlugin"
-    // plugin.constructor.name !== "ForkTsCheckerWebpackPlugin"
   );
-  // paths.appTsConfig = void 0; // 关闭`TS`校验插件 在`VSCode`校验
   // 添加插件
   process.env.NODE_ENV === "production" &&
     config.plugins.push(
       new UglifyJsPlugin({
         uglifyOptions: {
-          compress: {
-            drop_debugger: true,
-            drop_console: true,
-          },
+          compress: { drop_debugger: true, drop_console: true },
         },
       })
     );
   return config;
 };
 
+const configEmpty = config => config;
+
 module.exports = {
   webpack: override(
-    // 不主动处理样式按需加载 主动维护依赖项 作为`npm`包使用
-    // fixBabelImports("@arco-design/web-react", {
-    //   libraryDirectory: "es",
-    //   camel2DashComponentName: false,
-    //   style: true,
-    // }),
-    // fixBabelImports("@arco-design/web-react/icon", {
-    //   libraryDirectory: "react-icon",
-    //   camel2DashComponentName: false,
-    // }),
+    process.env.NODE_ENV === "development" ? rewiredESbuild() : configEmpty,
     addLessLoader({
       javascriptEnabled: true,
       importLoaders: true,
