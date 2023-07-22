04eaeb00260a214a2eeacb4f723a18710df29da8
WindrunnerMax <651525974@qq.com>
2023-07-22 18:00:27 +0800

    chore: viod key set

--- src/core/schema/index.ts
+++ src/core/schema/index.ts
@@ -1,20 +1,22 @@
 import { Editor } from "slate";
 
 type SchemaItem = {
-  isVoid: boolean;
+  isVoid?: boolean;
 };
 export type SlateSchema = Record<string, SchemaItem>;
 
 export const withSchema = <T extends Editor>(schema: SlateSchema, editor: T): T => {
   const { isVoid } = editor;
-  const voidKeys: string[] = [];
+
+  const voidKeys: Set<string> = new Set<string>();
+
   for (const [key, value] of Object.entries(schema)) {
-    if (value.isVoid) voidKeys.push(key);
+    if (value.isVoid) voidKeys.add(key);
   }
 
   editor.isVoid = element => {
-    for (const key of voidKeys) {
-      if (element[key]) return true;
+    for (const key of Object.keys(element)) {
+      if (voidKeys.has(key)) return true;
     }
     return isVoid(element);
   };
