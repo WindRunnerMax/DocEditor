9386cfa0123c6389169f60bafac18efe3622a242
WindrunnerMax <651525974@qq.com>
2023-10-22 18:00:03 +0800

    fix: style & next line insert

--- src/plugins/doc-toolbar/utils/exec.ts
+++ src/plugins/doc-toolbar/utils/exec.ts
@@ -1,6 +1,5 @@
 import { ReactEditor } from "slate-react";
 import { DocToolBarState } from "../types";
-import { focusSelection } from "src/core/ops/set";
 import { execCommand } from "src/core/command";
 import { Transforms } from "slate";
 
@@ -8,9 +7,13 @@ export const exec = (state: DocToolBarState, key: string) => {
   const { editor, element, commands } = state;
   const [type, data] = key.split(".");
   const path = ReactEditor.findPath(editor, element);
-  focusSelection(editor, path);
+  ReactEditor.focus(editor);
   if (state.status.isNextLine) {
-    Transforms.insertNodes(editor, { children: [{ text: "" }] });
+    if (!path.length) return void 0;
+    const nextPath = path.slice(0, -1);
+    const lastPath = path[path.length - 1] + 1;
+    nextPath.push(lastPath);
+    Transforms.insertNodes(editor, { children: [{ text: "" }] }, { at: nextPath, select: true });
     Promise.resolve().then(() => {
       const selection = state.editor.selection;
       if (!selection) return void 0;
