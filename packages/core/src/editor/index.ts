cf613c86830a8612f209e381e60fa887707631c7
WindrunnerMax <651525974@qq.com>
2024-03-29 21:28:43 +0800

    chore: annotation

--- packages/core/src/editor/index.ts
+++ packages/core/src/editor/index.ts
@@ -13,3 +13,9 @@ export function makeEditor(schema: EditorSchema) {
   // `editor.apply`可以处理`Op&Selection Change` 重新分发事件
   return withSchema(schema, editor) as EditorSuite;
 }
+
+/**
+ * 1. 完整封装组件 通过`Editor`重新分发事件
+ * 2. 重新设计数据结构 避免大量层级嵌套 扁平化数据结构
+ * 3. 增加状态管理 `Blocks`级数据结构管理
+ */
